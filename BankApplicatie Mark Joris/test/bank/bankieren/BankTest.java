9d6b696afcb75785c9356989872e3deb11652f32
MarkClaessens <mark-claessens@hotmail.com>
2016-01-05 00:12:48 +0100

    testenzo

--- BankApplicatie Mark Joris/test/bank/bankieren/BankTest.java
+++ BankApplicatie Mark Joris/test/bank/bankieren/BankTest.java
@@ -5,6 +5,9 @@
  */
 package bank.bankieren;
 
+import fontys.util.NumberDoesntExistException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -83,5 +86,68 @@ public class BankTest {
         IRekening testRekening = new Rekening(100000000, new Klant("Joris", "Oploo"), "Euro");
         bank.openRekening("Joris", "Oploo");
         assertTrue("Rekeningen wordt niet goed opgehaald", testRekening.equals(bank.getRekening(100000000)));
+    }
+    
+    @Test
+    public void maakoverzelfnummer() throws RuntimeException
+    {
+      Money geld = new Money(20, "euro");
+        try {
+            bank.maakOver(100000001, 100000001, geld);
+        } catch (NumberDoesntExistException ex) {
+            Logger.getLogger(BankTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Test
+    public void maakovernietpositief() throws RuntimeException
+    {
+      Money geld = new Money(-20, "euro");
+        try {
+            bank.maakOver(100000001, 100000002, geld);
+        } catch (NumberDoesntExistException ex) {
+            Logger.getLogger(BankTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Test
+    public void maakovernietbestaandown() throws NumberDoesntExistException
+    {
+      Money geld = new Money(20, "euro");
+        try {
+            bank.maakOver(100000001, 100000002, geld);
+        } catch (NumberDoesntExistException ex) {
+            Logger.getLogger(BankTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Test
+    public void maakovernietbestaanddest() throws NumberDoesntExistException
+    {     
+      bank.openRekening("Joris", "Oploo");
+      Money geld = new Money(20, "euro");
+        try {
+            bank.maakOver(100000000, 100000002, geld);
+        } catch (NumberDoesntExistException ex) {
+            Logger.getLogger(BankTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @Test
+    public void maakover()
+    {
+      bank.openRekening("Joris", "Oploo");
+      bank.openRekening("Mark", "Castenray");
+      bank.openRekening("Tom", "Jones");
+      Money geld1 = new Money(20, "euro");
+      Money geld2 = new Money(100, "euro");
+      Money geld3 = new Money(40, "euro");
+        try {
+            assertTrue("niet overgemaakt",bank.maakOver(100000000, 100000001, geld1));
+            assertTrue("niet overgemaakt",bank.maakOver(100000000, 100000002, geld2));
+            assertTrue("niet overgemaakt",bank.maakOver(100000001, 100000002, geld3));
+            assertTrue("niet overgemaakt",bank.maakOver(100000002, 100000001, geld1));
+            assertTrue("niet overgemaakt",bank.maakOver(100000002, 100000000, geld2));
+            assertTrue("niet overgemaakt",bank.maakOver(100000001, 100000000, geld3));
+        } catch (NumberDoesntExistException ex) {
+            Logger.getLogger(BankTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- BankApplicatie Mark Joris/test/bank/internettoegang/BalieTest.java
+++ BankApplicatie Mark Joris/test/bank/internettoegang/BalieTest.java
@@ -5,6 +5,10 @@
  */
 package bank.internettoegang;
 
+import bank.bankieren.Bank;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,6 +21,9 @@ import static org.junit.Assert.*;
  * @author jvdwi
  */
 public class BalieTest {
+    
+    Bank bank;
+    Balie balie;
     
     public BalieTest() {
     }
@@ -31,6 +38,12 @@ public class BalieTest {
     
     @Before
     public void setUp() {
+        bank = new Bank("rabo");
+        try {
+            balie = new Balie(bank);
+        } catch (RemoteException ex) {
+            Logger.getLogger(BalieTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @After
@@ -40,6 +53,38 @@ public class BalieTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void openrekening(){
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("Tom", "", ""));
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("", "Castenray", ""));
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("", "", "Cavia"));
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("Tom", "Castenray", ""));
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("", "Castenray", "Cavia"));
+    assertNull("leeg invoerveld niet afgevangen", balie.openRekening("Tom", "", "Cavia"));
+    assertNotNull("geen random getal teruggekregen", balie.openRekening("Tom", "Castenray", "Cavia"));
+    assertNotNull("geen random getal teruggekregen", balie.openRekening("Henk", "Oirlo", "Hamster"));
+    assertNotNull("geen random getal teruggekregen", balie.openRekening("Kees", "Valkenswaard", "Konijn"));
+    assertNotNull("geen random getal teruggekregen", balie.openRekening("fred", "Eindhoven", "fredje"));
+    }
+    
+    @Test
+    public void login(){
+        balie.openRekening("Tom", "Castenray", "Cavia");
+        balie.openRekening("Henk", "Oirlo", "Hamster");
+        balie.openRekening("Kees", "Valkenswaard", "Konijn");
+        balie.openRekening("fred", "Eindhoven", "fredje");
+        try {
+            assertNull("account niet gevonden", balie.logIn("", ""));
+            assertNull("account niet gevonden", balie.logIn("Mark", ""));
+            assertNull("password incorrect", balie.logIn("Tom", ""));
+            assertNull("password incorrect", balie.logIn("Tom", "Hamster"));
+            assertNull("password incorrect", balie.logIn("Kees", "fredje"));
+            assertNotNull("account niet ingelogd", balie.logIn("Tom", "Cavia"));
+            assertNotNull("account niet ingelogd", balie.logIn("Henk", "Hamster"));
+            assertNotNull("account niet ingelogd", balie.logIn("Kees", "Konijn"));
+            assertNotNull("account niet ingelogd", balie.logIn("fred", "fredje"));
+        } catch (RemoteException ex) {
+            Logger.getLogger(BalieTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
